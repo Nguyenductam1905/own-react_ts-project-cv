497729688e6af3dacd3f31b7a87a940087354403
Windows 10 <horashidev@gmail.com>
2025-04-21 07:14:32 +0700

    pc1

---
+++ src/components/layout/app.footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <>
+            align center, justify center, align-items center,
+            <div className="text-center my-3">
+                sdfdf
+            </div>
+        </>
+    )
+}
+
+export default Footer
